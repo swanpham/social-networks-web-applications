35d74ad186be7b4b8930125bdb049d4a9cd16ad7
Swan Pham <accounting@nailsrus.ca>
2021-06-25 22:10:55 -0400

    add users routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,2 +1,32 @@
 const router = require( 'express' ).Router();
+
+const {
+    getUsers,
+    addUser,
+    getOneUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+
+
+} = require ('../../controllers/user-controller');
+
+router
+.route('/')
+.get(getUsers)
+.post(addUser)
+
+router
+.route('/:id')
+.get(getOneUser)
+.put(updateUser)
+.delete(deleteUser)
+
+router 
+    .route( '/:id/friends/:friendId' )
+    .post(addFriend)
+    .delete(removeFriend)
+
+
 module.exports = router;
